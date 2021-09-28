1e434da928a11f0e5c0aac996ac011249b483bbb
darrinGone101 <maxe138@deltalearns.ca>
2021-09-28 14:13:52 -0700

    done

--- task1.py
+++ task1.py
@@ -26,6 +26,6 @@ That is within normal Earth orbit.
 """
 num1 = float(input("enter a number: "))
 if num1 >= 0.9759 and num1 <= 1.016:
-    print("That is within the Earths orbit.")
+    print("That is within normal Earths orbit.")
 else:
     print("That is not within normal Earths orbit.")
